b79e61662d92ca3d55a242e446d2443a8a2996b1
Andrew Balakirev <balakirev.andrey@gmail.com>
2018-06-19 14:34:04 +0300

    hot reload replaced with watchmen

--- src/api/hello.js
+++ src/api/hello.js
@@ -6,16 +6,6 @@ const router = new Router();
 router.get("/", hello);
 /* eslint-enable no-use-before-define */
 
-/**
- * @api {delete} /api/address/:id Delete address
- * @apiName DeleteAddress
- * @apiGroup Address
- * @apiUse ValidatedResponse
- *
- * @apiParam (GET params) {UUID} id
- * @apiSuccess (Success 204) 204 Empty Response
- * @apiError 404 Address is not found
- */
 async function hello(req, res, next) {
   try {
     res.status(200).send("Hello!");
--- src/server.js
+++ src/server.js
@@ -1,67 +1,27 @@
-const { writeFile: writeFileAsync } = require("fs");
-const { promisify } = require("util");
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const pinoHttp = require("pino-http");
 const logger = require("./tools/logger");
 const config = require("./config/config");
 
-const writeFile = promisify(writeFileAsync);
+// Configure HTTP server
+const server = express();
 
-let app;
-let server;
-let reload = Promise.resolve();
+// Register Node.js/Express middleware
+server.use(cookieParser());
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+server.use(pinoHttp({ logger }));
 
-// Launch Node.js server
-function launch(callback) {
-  app = require("./app"); // eslint-disable-line global-require
-  app.set("port", config.app.port);
-  server = app.listen(app.get("port"), () => {
-    logger.info(
-      `The server is running at http://localhost:${app.get("port")}/`
-    );
-    if (callback) callback();
-  });
-}
+server.use("/", require("./api/hello"));
 
-// Shutdown Node.js server and database clients
-async function shutDown() {
-  const stats = app.get("stats");
-  if (stats) {
-    await writeFile("stats.txt", stats.join("\n"), "utf8");
-  }
+server.get("/*", (req, res) => res.sendStatus(404));
+server.use(require("./middlewares/errorHandler"));
 
-  if (server) {
-    await new Promise(resolve =>
-      server.close(() => {
-        resolve();
-      })
-    );
-  }
-}
+const { port } = config.app;
+server.listen(port, () => {
+  logger.info(`The server is running at http://localhost:${port}/`);
+});
 
-function handleError(err) {
-  logger.error(err.stack);
-}
-
-// Graceful shutdown
-process.once("SIGTERM", () => shutDown().then(() => process.exit()));
-
-// In development mode the app is launched with an IPC channel
-if (process.channel) {
-  // Prevent exiting the process in development mode
-  process.on("uncaughtException", handleError);
-  // Restart the server on code changes (see scripts/run.js)
-  process.on("message", message => {
-    if (message === "reload") {
-      reload = reload.then(() => shutDown()).then(() => {
-        Object.keys(require.cache).forEach(key => {
-          if (key.indexOf("node_modules") === -1) delete require.cache[key];
-        });
-        return new Promise(resolve => launch(resolve)).catch(handleError);
-      });
-    }
-  });
-  process.on("disconnect", () => process.emit("SIGTERM"));
-}
-
-process.on("unhandledRejection", handleError);
-
-launch();
+module.exports = server;
