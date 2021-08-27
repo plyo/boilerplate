ebdc19ae7e1572a4e231e0576575db3768500eb6
Andrew Balakirev <balakirev.andrey@gmail.com>
2021-08-27 17:56:58 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,11 @@
 # as print prettified logs into stdout. Here we create named file descriptor to pipe logs into it with help of `tee`.
 # These logs are prettified by pino and goes into stdout because of `cat`
 pipe=/tmp/logs_pipe
-if [[ ! -p ${pipe} ]]; then
+if [ ! -p "${pipe}" ]; then
   mkfifo ${pipe}
 fi
 
-cat ${pipe} | ./node_modules/.bin/pino &
+< ${pipe} ./node_modules/.bin/pino &
 
 # tee forks logs into `$pipe` and elasticsearch
-node src/server.js | tee ${pipe} | ./node_modules/.bin/pino-elasticsearch --host ${ELASTIC_HOST}
+node build/server.js | tee ${pipe} | ./node_modules/.bin/pino-elasticsearch --host "${ELASTIC_HOST}"
