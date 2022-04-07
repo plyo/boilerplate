e193102d66b6933a7a0cce2cc94b78c2b1a27565
Vladimir Kutepov <frenzzy.man@gmail.com>
2022-04-07 16:28:10 +0300

    Release notes without gren.sh (#18)

--- gren.sh
+++
@@ -1,1 +0,0 @@
-gren release --username=plyo --repo=boilerplate --override && RES=`OCTOKIT_GITHUB_TOKEN=$GREN_GITHUB_TOKEN prepareTags --repo boilerplate` && gren release --data-source=commits --username=plyo --repo=boilerplate --override --tags=$RES
